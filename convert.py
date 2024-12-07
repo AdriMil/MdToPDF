e1eab326b8443e23ebba2bb99cb4f7ea465221a7
adrien <adrien.millot@yahoo.fr>
2024-12-07 13:34:01 +0100

    main file ready

--- convert.py
+++ convert.py
@@ -1,7 +1,15 @@
-import sys
 from markdown_pdf import MarkdownPdf, Section
+from gather_files import exclude_files, findMdFiles, createUniqueMdFile
 
-pdf = MarkdownPdf(toc_level=4)
-pdf.add_section(Section(open(sys.argv[1], encoding='utf-8').read()))
+file_name_without_extention = "Documentation"
+md_file_name = file_name_without_extention + ".md"
+pdf_file_name = file_name_without_extention + ".pdf"
+
+all_my_md_files = findMdFiles()
+my_md_files = exclude_files(files_list=all_my_md_files)
+createUniqueMdFile(file_list=my_md_files, my_file_name=md_file_name)
+
+pdf = MarkdownPdf(toc_level=False)
+pdf.add_section(Section(open(md_file_name, encoding='utf-8').read()))
 pdf.meta["title"] = "MarkdownPdf module"
-pdf.save(sys.argv[2])
+pdf.save("pdf_documentation/" + pdf_file_name)

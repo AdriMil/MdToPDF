0abaaf02a0b929d4cb474736c56046cc0b93bdf2
adrien <adrien.millot@yahoo.fr>
2024-12-07 13:35:02 +0100

    functions file done

--- gather_files.py
+++ gather_files.py
@@ -9,13 +9,9 @@ def exclude_files(files_list: list, files_to_exlude: list =[])-> list:
                         if not any(word in file for word in files_to_exlude)]
     return filtered_list
 
-def createUniqueMdFile(file_list: list):
-    with open('Documentation.md', 'w', encoding='utf-8') as fout:
+def createUniqueMdFile(file_list: list, my_file_name: str):
+    with open(my_file_name, 'w', encoding='utf-8') as fout:
         for file_path in file_list:
             with open(file_path, 'r', encoding='utf-8') as fin:
                 fout.write(fin.read())
                 fout.write('\n')
-
-all_my_md_files = findMdFiles()
-my_md_files = exclude_files(all_my_md_files,['Documentation.md'])
-createUniqueMdFile(my_md_files)

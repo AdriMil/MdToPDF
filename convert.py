8ac84dff396c37a7d6b63056624a11e58924af08
adrien <adrien.millot@yahoo.fr>
2024-12-07 17:17:08 +0100

    Comments added

--- convert.py
+++ convert.py
@@ -1,15 +1,17 @@
 from markdown_pdf import MarkdownPdf, Section
 from gather_files import exclude_files, findMdFiles, createUniqueMdFile
 
+#VARIABLES
 file_name_without_extention = "Documentation"
 md_file_name = file_name_without_extention + ".md"
 pdf_file_name = file_name_without_extention + ".pdf"
 
-all_my_md_files = findMdFiles()
-my_md_files = exclude_files(files_list=all_my_md_files)
-createUniqueMdFile(file_list=my_md_files, my_file_name=md_file_name)
+
+all_my_md_files = findMdFiles()                                             #Search for all .md file
+my_md_files = exclude_files(files_list=all_my_md_files)                     #Exlude some file if needed
+createUniqueMdFile(file_list=my_md_files, my_file_name=md_file_name)        #Create a unique .md file
 
 pdf = MarkdownPdf(toc_level=False)
 pdf.add_section(Section(open(md_file_name, encoding='utf-8').read()))
-pdf.meta["title"] = "MarkdownPdf module"
-pdf.save("pdf_documentation/" + pdf_file_name)
+pdf.meta["title"] = "MarkdownPdf module"                                    
+pdf.save("pdf_documentation/" + pdf_file_name)                              #Pdf creation
--- gather_files.py
+++ gather_files.py
@@ -1,15 +1,38 @@
 import glob
 
 def findMdFiles(search_criteria: str ='./**/*.md')-> list:
+    """Search all files with .md extensions in current path and sub paths.
+
+    Args:
+        search_criteria (str, optional): Search criteria. Defaults to './**/*.md'.
+
+    Returns:
+        list: path to files which fit the search
+    """
     files = (glob.glob(search_criteria, recursive=True))
     return files
 
 def exclude_files(files_list: list, files_to_exlude: list =[])-> list:
+    """Revome froma a list some files by searching keyword. For example if you do not want include some ".md" file in the final pdf.
+
+    Args:
+        files_list (list): Liste with path and files names that are going to be merge in 1 md file.
+        files_to_exlude (list, optional): keywork that will be search and delete from "files_list" list . Defaults to [].
+
+    Returns:
+        list: _description_
+    """
     filtered_list = [file for file in files_list 
                         if not any(word in file for word in files_to_exlude)]
     return filtered_list
 
 def createUniqueMdFile(file_list: list, my_file_name: str):
+    """Will merge all ".md" files from input in only one ".md" file.
+
+    Args:
+        file_list (list): Liste with path and files names that are going to be merge in 1 md file.
+        my_file_name (str): created new .md file name that contain all ".md" files merged.
+    """
     with open(my_file_name, 'w', encoding='utf-8') as fout:
         for file_path in file_list:
             with open(file_path, 'r', encoding='utf-8') as fin:
